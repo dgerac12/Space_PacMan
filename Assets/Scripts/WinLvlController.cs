e79e01b75cc6e8d40960d88c4801ea258886cebd
Danielle Geraci <daniellegeraci@knights.ucf.edu>
2020-01-24 19:27:42 -0500

    initial commit

---
+++ Assets/Scripts/WinLvlController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLvlController : MonoBehaviour
+{
+    public GameObject lvlOne;
+    public GameObject lvlTwo;
+    public GameObject player;
+
+    public GameObject uiManager;
+    public bool lvlTwoStarted = false;
+
+    private PlayerController playerController;
+
+    private UIController uiController;
+
+    private void Start()
+    {
+        playerController = player.GetComponent<PlayerController>();
+        uiController = uiManager.GetComponent<UIController>();
+    }
+    void Update()
+    {
+        if(lvlTwoStarted == true)
+        {
+            playerController.lives = 2;
+            playerController.starCount = 0;
+            uiController.totalStars = 226;
+            lvlOne.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
+    }
+    public void GoToLvlTwo()
+    {
+        lvlTwo.SetActive(true);
+        lvlTwoStarted = true;
+        playerController.transform.position = playerController.playerStart;
+    }
+}
